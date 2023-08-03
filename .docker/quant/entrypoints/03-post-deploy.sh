f3f8686d4bb343cb1fec29353ebaa23e86186821
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-08-04 09:27:45 +1000

    Ensure database creation prior to installation.

--- .docker/quant/entrypoints/03-post-deploy.sh
+++ .docker/quant/entrypoints/03-post-deploy.sh
@@ -11,8 +11,9 @@ if [ "$QUANT_ENVIRONMENT_TYPE" == "development" ]; then
     # Here we perform a fresh installation, and install the required default modules.
     # We install devel_generate to create some sample content.
     if [ "$(jq -r '.bootstrap' 2> /dev/null <<< "$STATUS")" != "Successful" ]; then
-        drush si -y
-        drush pm-enable quant_nextjs devel_generate -y
-        drush devel-generate:content
+        drush sql:create -y
+        drush site:install -y
+        drush pm:enable quant_nextjs devel_generate -y
+        drush devel-generate:content -y
     fi
 fi
