aaa5c0706ad40e3dad6e9e7d33fe0732f5acbbd8
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-08-03 12:52:08 +1000

    Ignore lint during build. Use a consistent build id for static push.

--- src-nextjs/next.config.js
+++ src-nextjs/next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [process.env.NEXT_PUBLIC_IMAGE_DOMAIN, 'localhost'],
+    domains: ['localhost', process.env.NEXT_PUBLIC_IMAGE_DOMAIN],
     minimumCacheTTL: 2592000,
     unoptimized: true
+  },
+  eslint: {
+    // The linting process runs in CI rather than during build.
+    ignoreDuringBuilds: true,
+  },
+  generateBuildId: async () => {
+    return 'quant-static'
   },
 }
 
