b701f7432d01e6ac3f9bdc48194cd6f6e3474118
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-10-11 07:50:11 +1000

    Resolve hydration warnings on dates.

--- src-nextjs/components/node--article--teaser.tsx
+++ src-nextjs/components/node--article--teaser.tsx
@@ -21,7 +21,7 @@ export function NodeArticleTeaser({ node, ...props }: NodeArticleTeaserProps) {
             <span className="font-semibold">{node.uid?.display_name}</span>
           </span>
         ) : null}
-        <span> - {formatDate(node.created)}</span>
+        <span suppressHydrationWarning> - {formatDate(node.created)}</span>
       </div>
       {node.field_image && (
         <figure className="my-4">
